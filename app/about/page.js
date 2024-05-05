da6d5e972c0143e5dfe466e6367af8eb76fb7d83
jklama <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>
2024-05-06 02:16:06 +0545

    adding plugins and upto 23

---
+++ app/about/page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <Link href="/" className="text-2xl">
+        homePage
+      </Link>
+    </div>
+  );
+};
+
+export default About;
